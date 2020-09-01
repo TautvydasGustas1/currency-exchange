ba277b57d6e3c51f9796d8e8635eed972224fb5e
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-09-01 20:36:48 +0300

    update

--- Tests/DataAdded.test.js
+++
@@ -1,6 +0,0 @@
-const { getCurrenciesURL, getRatesURL } = require("../Utils/Constants");
-const DataAdder = require("../Utils/DataAdder");
-
-test("Gets content from certain request", () => {
-  expect((1 + 2).toBe(3));
-});
