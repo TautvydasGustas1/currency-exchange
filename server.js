a97f689577930f5ee06cf04028fd08b36fbb589c
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-09-01 20:55:27 +0300

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const connectDB = require("./config/db");
-const cookieParser = require("cookie-parser");
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 //Connect to DB
 connectDB();
@@ -10,10 +10,19 @@ connectDB();
 app.use(cookieParser());
 
 //Test route
-app.get("/api/", (req, res) => res.send("API RUNNING"));
+app.get('/api/', (req, res) => res.send('API RUNNING'));
 
 //Defined routes
-app.use("/api/exchange", require("./routes/api/exchange"));
+app.use('/api/exchange', require('./routes/api/exchange'));
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //Start server
 const PORT = process.env.PORT || 5000;
